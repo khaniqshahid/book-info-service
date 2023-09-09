1a3f1c1c017f4db4f17f12b1df03f5fe364799f1
Shahid Khan <khanshahidiqbal@gmail.com>
2023-09-09 11:42:17 +0000

    update-2 error

---
+++ errs/errors.go
@@ -0,0 +1,29 @@
+package errs
+
+import "net/http"
+
+type AppError struct {
+	Code    int
+	Message string
+}
+
+// // Error implements error.
+// func (*AppError) Error() string {
+// 	panic("unimplemented")
+// }
+
+func NewNotFoundError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusNotFound,
+	}
+
+}
+
+func NewUnexpectedError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusInternalServerError,
+	}
+
+}
